42cd6be604dfc31a1aa028bef3d94309aef74fd4
AlecCromer <33764772+AlecCromer@users.noreply.github.com>
2018-10-18 10:09:38 -0400

    Updated Javascript and HTML
    Added new website design to index and added idle timeout feature.

--- website/index.php
+++ website/index.php
@@ -1,17 +1,120 @@
+<!DOCTYPE html>
 <html>
 <head>
-<title>SecurePoll</title>
+<title>Home</title>
 <meta charset="UTF8">
-<link rel="stylesheet" href="securePoll.css">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<style>
+body {
+  margin: 0;
+  font-family: "Lato", sans-serif;
+}
+
+body {
+  font-size: 120%;
+  background-image:url("img/vote3.gif");
+  
+  background-size: cover;
+
+}
+
+
+.sidebar {
+  margin: 0;
+  padding: 0;
+  width: 200px;
+  background-color: #f1f1f1;
+  position: fixed;
+  height: 100%;
+  overflow: auto;
+}
+
+.sidebar a {
+  display: block;
+  color: black;
+  padding: 16px;
+  text-decoration: none;
+}
+ 
+.sidebar a.active {
+  background-color: #4CAF50;
+  color: white;
+}
+
+.sidebar a:hover:not(.active) {
+  background-color: #555;
+  color: white;
+}
+
+div.content {
+  margin-left: 200px;
+  padding: 1px 16px;
+  height: 1000px;
+}
+
+@media screen and (max-width: 700px) {
+  .sidebar {
+    width: 100%;
+    height: auto;
+    position: relative;
+  }
+  .sidebar a {float: left;}
+  div.content {margin-left: 0;}
+}
+
+@media screen and (max-width: 400px) {
+  .sidebar a {
+    text-align: center;
+    float: none;
+  }
+}
+</style>
+
 </head>
+<script>
+//timeout after 5 minutes
+attachEvent(window,'load',function(){
+  var idleSeconds =300;
+  var idleTimer;
+  function resetTimer(){
+    clearTimeout(idleTimer);
+    idleTimer = setTimeout(whenUserIdle,idleSeconds*1000);
+  }
+  attachEvent(document.body,'mousemove',resetTimer);
+  attachEvent(document.body,'keydown',resetTimer);
+  attachEvent(document.body,'click',resetTimer);	
+
+  resetTimer(); // Start the timer when the page loads
+});
+
+function whenUserIdle(){
+
+alert("You have been idle for 5 minutes, returning to home page.");
+document.location.href = "http://localhost/SecurePoll/index.php";
+}
+
+function attachEvent(obj,evt,fnc,useCapture){
+  if (obj.addEventListener){
+    obj.addEventListener(evt,fnc,!!useCapture);
+    return true;
+  } else if (obj.attachEvent){
+    return obj.attachEvent("on"+evt,fnc);
+  }
+} 
+</script>
 <body>
-<div>
-<h2>Welcome to SecurePoll</h2>
-<p><a href="register.php">Register Here</a></p>
-<p><a href="login.php">Login Here</a></p>
+<div class="sidebar">
+  <a class="active" href="#home">Home</a>
+  <a href="login.php">LOGIN</a>
+  <a href="register.php">REGISTER</a>
+ 
+</div>
+
+<div class="content">
+  <h2>WELCOME TO SECURE POLL</h2>
+  <p>To navigate on this website simply click on the links on your left side of the screen</p>
+  
+  
 </div>
 </body>
 <html>
-<script>
-
-</script>

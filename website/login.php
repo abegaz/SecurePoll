7e3902c08c114566f9856cd3bdfc86bdc706a003
AlecCromer <33764772+AlecCromer@users.noreply.github.com>
2018-10-22 22:29:03 -0400

    Update login.php

--- website/login.php
+++ website/login.php
@@ -4,7 +4,6 @@
   <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <script type="text/javascript">
-
 //timeout after 5 minutes
 attachEvent(window,'load',function(){
   var idleSeconds =300;
@@ -16,16 +15,12 @@ attachEvent(window,'load',function(){
   attachEvent(document.body,'mousemove',resetTimer);
   attachEvent(document.body,'keydown',resetTimer);
   attachEvent(document.body,'click',resetTimer);	
-
   resetTimer(); // Start the timer when the page loads
 });
-
 function whenUserIdle(){
-
 alert("You have been idle for 5 minutes, returning to home page.");
 document.location.href = "http://localhost/SecurePoll/index.php";
 }
-
 function attachEvent(obj,evt,fnc,useCapture){
   if (obj.addEventListener){
     obj.addEventListener(evt,fnc,!!useCapture);
@@ -42,62 +37,9 @@ function attachEvent(obj,evt,fnc,useCapture){
 		<form id="login" action="authenticate.php" method="POST">
 			<div class="input-group"><label>First Name</label><input  name="fName" type="text" placeholder="Enter First Name" required></div>
 			<div class="input-group"><label>Last name</label><input  name="Lname" type="text" placeholder="Enter Last Name" required></div>
-			<div class="input-group"><label>Date Of Birth</label><input  name="DoB" type="date" placeholder="Date of Birth" required></div>
 			<div class="input-group"><label>Social Security Number</label><input  name="ssn" type="number" placeholder="Last 4 digits of your Social Security Number" required></div>
-			<div class="input-group"><label>Password</label><input  name="VoterIDNum" type="number" placeholder="Voter ID Number" required></div>
-			<div class="input-group"><label>State</label><select name="state" required>
-				<option value="AL">Alabama</option>
-				<option value="AK">Alaska</option>
-				<option value="AZ">Arizona</option>
-				<option value="AR">Arkansas</option>
-				<option value="CA">California</option>
-				<option value="CO">Colorado</option>
-				<option value="CT">Connecticut</option>
-				<option value="DE">Delaware</option>
-				<option value="DC">District Of Columbia</option>
-				<option value="FL">Florida</option>
-				<option value="GA">Georgia</option>
-				<option value="HI">Hawaii</option>
-				<option value="ID">Idaho</option>
-				<option value="IL">Illinois</option>
-				<option value="IN">Indiana</option>
-				<option value="IA">Iowa</option>
-				<option value="KS">Kansas</option>
-				<option value="KY">Kentucky</option>
-				<option value="LA">Louisiana</option>
-				<option value="ME">Maine</option>
-				<option value="MD">Maryland</option>
-				<option value="MA">Massachusetts</option>
-				<option value="MI">Michigan</option>
-				<option value="MN">Minnesota</option>
-				<option value="MS">Mississippi</option>
-				<option value="MO">Missouri</option>
-				<option value="MT">Montana</option>
-				<option value="NE">Nebraska</option>
-				<option value="NV">Nevada</option>
-				<option value="NH">New Hampshire</option>
-				<option value="NJ">New Jersey</option>
-				<option value="NM">New Mexico</option>
-				<option value="NY">New York</option>
-				<option value="NC">North Carolina</option>
-				<option value="ND">North Dakota</option>
-				<option value="OH">Ohio</option>
-				<option value="OK">Oklahoma</option>
-				<option value="OR">Oregon</option>
-				<option value="PA">Pennsylvania</option>
-				<option value="RI">Rhode Island</option>
-				<option value="SC">South Carolina</option>
-				<option value="SD">South Dakota</option>
-				<option value="TN">Tennessee</option>
-				<option value="TX">Texas</option>
-				<option value="UT">Utah</option>
-				<option value="VT">Vermont</option>
-				<option value="VA">Virginia</option>
-				<option value="WA">Washington</option>
-				<option value="WV">West Virginia</option>
-				<option value="WI">Wisconsin</option>
-				<option value="WY">Wyoming</option>
-		</select></div>	
+			<div class="input-group"><label>Voter ID Number</label><input  name="VoterIDNum" type="text" placeholder="Enter Last Name" required></div>
+			<div class="input-group"><label>Password</label><input  name="password" type="password" placeholder="Password" required></div>
 			<div class="input-group"><button type="Submit" class="btn" name="RegisterButton">Login</button></div>
   	<p>
   		Not a member? <a href="register.php">Sign up</a>
